294324fa0a694f1e4a75fac82cf46f5b49230d59
Cengiz Gunay <cengique@users.sf.net>
2002-04-15 23:40:18 +0000

    Initial version. Works.

---
+++ src/edu/ull/cgunay/plots/ProfilePlot.java
@@ -0,0 +1,42 @@
+
+package neuroidnet.ntr.plots;
+
+// $Id$
+/**
+ * Plots <code>Profile</code>s.
+ *
+ * 
+ * <p>Created: Sat Apr 13 14:47:44 2002
+ * <p>Modified: $Date$
+ *
+ * @see Profile
+ * @author <a href="mailto:">Cengiz Gunay</a>
+ * @version $Revision$ for this file.
+ */
+
+public class ProfilePlot extends Plot  {
+    Profile profile;
+
+    public ProfilePlot (String title, Range range, Profile profile) {
+	super(title, range);
+
+	this.profile = profile;
+	
+	if (range == null) 
+	    this.range = profile.getRange();
+    }
+
+    public ProfilePlot (Profile profile) {
+	this(null, null, profile);
+    }
+
+    /**
+     * Creates a profile string changing with variable "t". 
+     * @see Grapher#profile
+     * @return a <code>String</code> value
+     */
+    public String body() { 
+	return grapher.profile(profile, range); 
+    }
+    
+}// ProfilePlot
