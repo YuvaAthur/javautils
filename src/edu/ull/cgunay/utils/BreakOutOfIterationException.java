14a87b745acf8f403bd2f1fd46a128b400479ddd
Cengiz Gunay <cengique@users.sf.net>
2002-04-25 03:50:56 +0000

    Initial.

---
+++ src/edu/ull/cgunay/utils/BreakOutOfIterationException.java
@@ -0,0 +1,21 @@
+
+package neuroidnet.utils;
+
+// $Id$
+/**
+ * Thrown in iterators when the iteration needs to be terminated.
+ *
+ * <p>Created: Sat Apr 20 14:42:05 2002
+ * <p>Modified: $Date$
+ *
+ * @see Iteration
+ * @author <a href="mailto:">Cengiz Gunay</a>
+ * @version $Revision$ for this file.
+ */
+
+public class BreakOutOfIterationException extends IterationException  {
+    public BreakOutOfIterationException () {
+	
+    }
+    
+}// BreakOutOfIterationException
