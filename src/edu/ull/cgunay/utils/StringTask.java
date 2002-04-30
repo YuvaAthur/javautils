6e549be462df6fbf1978ada3c9e865195674a147
Cengiz Gunay <cengique@users.sf.net>
2002-04-30 03:54:31 +0000

    Added documentation and changed package names to edu.ull.cgunay.utils.* Uses new class UninterruptedIteration.

--- src/edu/ull/cgunay/utils/StringTask.java
+++ src/edu/ull/cgunay/utils/StringTask.java
@@ -1,13 +1,25 @@
 
-package neuroidnet.utils;
-import neuroidnet.utils.Task;
-import neuroidnet.utils.TaskWithReturn;
+package edu.ull.cgunay.utils;
+import edu.ull.cgunay.utils.Task;
+import edu.ull.cgunay.utils.TaskWithReturn;
 import java.util.Collection;
 // $Id$
 /**
- * Iteration task that keeps a <code>String</code> value.
- *
- *
+ * Iteration task that accumulates a <code>String</code> value. Constructors allow
+ * optionally starting and ending <code>String</code>s  to wrap the
+ * <code>String</code> created by iterating the <code>Collection</code>
+ * given to the <code>getString()</code> method.
+ * <p>Example to display contents of a <code>Vector</code> holding
+ * <code>double</code> values in parenthesis:
+ * <pre>
+ * String inParens = new StringTask("(", ")") {
+ *   public void job(Object o) {
+ *     if (!retval.equals("")) 
+ *       super.job(", ");	// Add a comma if there's any preceding entry
+ *     super("" + o);	// Add the string representation of object
+ *   }
+ * }.getString(doubleVector);	// Iterate on doubleVector
+ * </pre>
  * <p>Created: Tue Apr  9 13:40:54 2002
  * <p>Modified: $Date$
  *
@@ -37,7 +49,7 @@ public class StringTask implements TaskWithReturn {
     public StringTask () { }
 
     /**
-     * Initialize <code>retval</code> with argument.
+     * Initialize the accumulated <code>String</code> to the argument.
      *
      * @param initialRetval a <code>String</code> value
      */
@@ -46,13 +58,16 @@ public class StringTask implements TaskWithReturn {
     }
 
     /**
-     * Initialize retval and also accept an argument for <code>closing</code> that is added
-     * to retval before returning from <code>getValue()</code> or <code>getString()</code>.
+     * Wrap the accumulated <code>String</code> with an initial prefix and a final appendix.
+     * Initialize <code>retval</code> to <code>initialRetval</code> and
+     * <code>closing</code> to <code>closingAddition</code>. The latter is added
+     * to <code>retval</code> before being returned from
+     * <code>getValue()</code> or <code>getString()</code>.
+     * @param initialRetval assigned to <code>retval</code>
+     * @param closingAddition assigned to <code>closing</code>
      * @see #closing
      * @see #getValue
      * @see #getString
-     * @param initialRetval assigned to <code>retval</code>
-     * @param closingAddition assigned to <code>closing</code>
      */
     public StringTask (String initialRetval, String closingAddition) {
 	this(initialRetval);
@@ -60,19 +75,22 @@ public class StringTask implements TaskWithReturn {
     }
 
     /**
-     * Runs the iteration and returns a <code>String</code> value.
-     * @see #getValue()
+     * Iterates on the <code>list</code> and returns the accumulated <code>String</code>.
+     * Gracefully trims the items if a <code>BreakOutOfIterationException</code> is encountered
+     * during iteration.
      * @return a <code>String</code> value
+     * @see #getValue()
+     * @see UninterruptedIteration
      */
     public String getString(Collection list) {
-	Iteration.loop(list, this);
+	UninterruptedIteration.loop(list, this);
 	return (String)getValue(); 
     }
 
-    // implementation of neuroidnet.utils.TaskWithReturn interface
+    // implementation of edu.ull.cgunay.utils.TaskWithReturn interface
 
     /**
-     * Adds a closing string value.
+     * Return the accumulated <code>String</code> appended by the <code>closing</code>.
      * @see #closing
      * @return <description>
      */
@@ -80,11 +98,12 @@ public class StringTask implements TaskWithReturn {
 	return retval + closing;
     }
 
-    // implementation of neuroidnet.utils.Task interface
+    // implementation of edu.ull.cgunay.utils.Task interface
 
     /**
+     * Job to be done on each <code>Collection</code> item <code>o</code>.
      * By default adds the <code>String</code> representation of <code>o</code>
-     * to <code>retval</code>.
+     * to the accumulated <code>String</code>.
      * @param o the token of this iteration step
      */
     public void job(Object o) {
