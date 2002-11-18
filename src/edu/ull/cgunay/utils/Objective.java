82dd004cd63097d0418c3532cc183d5b43ff589b
Cengiz Gunay <cengique@users.sf.net>
2002-11-18 01:24:19 +0000

    Changed synchronization structure such that receiving an interrupt makes the thread gracefully exit.

--- src/edu/ull/cgunay/utils/Objective.java
+++ src/edu/ull/cgunay/utils/Objective.java
@@ -11,35 +11,35 @@ public class Objective implements Runnable {
     }
 
     public void run() {
-	while (true) {
-	    synchronized (lock) { 
-		lock.waitcount++;
-		try {
+	try {
+	    while (true) {
+		synchronized (lock) { 
+		    lock.waitcount++;
+
 		    //System.out.println(o + " waiting..");
 		    lock.wait();
-		} catch (InterruptedException e) {
-		    System.out.println("interrupted!!!" + e);
-		    e.printStackTrace();
+		    lock.waitcount--;
 		}
-		lock.waitcount--;
-	    }
 
-	    try {
-		task.job(o);		// Out of synchronized, therefore concurrent		 
-	    } catch (TaskException e) {
-		throw new Error("" + e);
-	    } // end of try-catch
+		try {
+		    task.job(o); // Out of synchronized, therefore concurrent		 
+		} catch (TaskException e) {
+		    throw new Error("" + e);
+		} // end of try-catch
 
-	    synchronized (lock) { 
-		lock.runcount--;
-		/*if (lock.runcount == 0) {
-		  System.out.println(o + " notified one!");
+		synchronized (lock) { 
+		    lock.runcount--;
+		    /*if (lock.runcount == 0) {
+		      System.out.println(o + " notified one!");
 			
-		  lock.notify(); // Wake up controller
-		  lock.reset();
-		  }*/ // end of if (lock.count == 0)
+		      lock.notify(); // Wake up controller
+		      lock.reset();
+		      }*/ // end of if (lock.count == 0)
 		    
-	    }
-	} // end of while (true)	    
+			}
+	    } // end of while (true)	    
+	} catch (InterruptedException e) {
+	    System.out.println("Received interrupt, exiting thread in + " + this);
+	}
     }
 }
